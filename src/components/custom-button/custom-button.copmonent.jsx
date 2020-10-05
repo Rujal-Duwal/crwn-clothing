67b3aaa4f8581411254ec5ebfb91c0d002b2b720
RujalDuwal <czar69@live.com>
2020-10-05 09:29:04 +0545

    updating our app to redirect to home if user is signed in

--- src/components/custom-button/custom-button.copmonent.jsx
+++ src/components/custom-button/custom-button.copmonent.jsx
@@ -5,6 +5,7 @@ import './custom-button.styles.scss'
 const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
     <button className={`custom-button  ${isGoogleSignIn ? 'google-sign-in' : ''}`} {...otherProps}>
         {children}
+        {console.log(children)}
     </button>
 );
 
